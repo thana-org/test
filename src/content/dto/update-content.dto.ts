e8b277835fe4bba5cc05da0fe7d53e481abc0f6b
Thanayut T <b@thanayut.in.th>
2022-10-13 19:15:29 +0700

    Update swagger documentation

--- src/content/dto/update-content.dto.ts
+++ src/content/dto/update-content.dto.ts
@@ -3,9 +3,9 @@ import { ApiProperty } from '@nestjs/swagger';
 export class UpdateContentDto {
   videoUrl: string;
 
-  @ApiProperty({ example: 'แนะนำเลยครับ' })
+  @ApiProperty({ example: 'เยี่ยมครับ', required: false })
   comment: string;
 
-  @ApiProperty({ example: 5.0, maximum: 5.0, minimum: 0.0 })
+  @ApiProperty({ example: 5.0, maximum: 5.0, minimum: 0.0, required: false })
   rating: number;
 }
